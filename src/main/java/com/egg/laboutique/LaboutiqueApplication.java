296d2284c432b60d8cfd63ad0ef6d22f4bdaeae9
LuxMG <lucecitagiuliani@gmail.com>
2021-11-25 20:36:47 -0300

    first commit

---
+++ src/main/java/com/egg/laboutique/LaboutiqueApplication.java
@@ -0,0 +1,13 @@
+package com.egg.laboutique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboutiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboutiqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/egg/laboutique/LaboutiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egg.laboutique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboutiqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
