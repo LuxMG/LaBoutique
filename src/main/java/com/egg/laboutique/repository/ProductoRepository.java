6e08c0e009d47d811065e04845dc6ae99889e19d
Mailen Cuzzoni <mailenycuzzoni@gmail.com>
2021-12-02 01:54:33 -0300

    Repository y service

--- src/main/java/com/egg/laboutique/repository/ProductoRepository.java
+++ src/main/java/com/egg/laboutique/repository/ProductoRepository.java
@@ -1,7 +1,4 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
  */
 package com.egg.laboutique.repository;
 
@@ -21,44 +18,65 @@ import org.springframework.stereotype.Repository;
 
 /**
  *
- * @author Mai
+ * @author Mailen
  */
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
     
     //Long id, String titulo, String descripcion, Tipo tipo, Estado estado, Categoria categoria, Foto foto, Usuario donante, Usuario beneficiario, Boolean alta, LocalDateTime creacion, LocalDateTime modificacion
    
+    //Para crear y modificar
     @Modifying
-    @Query("UPDATE Producto p SET p.titulo = :titulo, p.descripcion = :descripcion, p.tipo = :tipo, p.estado = :estado, p.categoria = :categoria, p.foto = foto, p.donante = :donante, p.beneficiario = :beneficiario, p.creacion = :creacion, p.modificacion = :modificacion WHERE l.id = :id")
-    void modificar(@Param("id") Long id, @Param("titulo") String titulo, @Param("descripcion") String descripcion, @Param("tipo") Tipo tipo, @Param("estado") Estado estado, @Param("categoria") Categoria categoria, @Param("foto") Foto foto, @Param("donante") Usuario donante, @Param("beneficiario") Usuario beneficiario, @Param("creacion") LocalDateTime creacion, @Param("modificacion") LocalDateTime modificacion);
+    @Query("UPDATE Producto p SET p.titulo = :titulo, "
+            + "p.descripcion = :descripcion, p.tipo = :tipo, "
+            + "p.estado = :estado, p.categoria = :categoria, "
+            + "p.foto = :foto, "
+            + "p.donante = :donante, p.beneficiario = :beneficiario, "
+            + "p.modificacion = :modificacion WHERE p.id = :id")
+    void modificar(@Param("id") Long id,
+            @Param("titulo") String titulo,
+            @Param("descripcion") String descripcion,
+            @Param("tipo") Tipo tipo, @Param("estado") Estado estado,
+            @Param("categoria") Categoria categoria,
+            @Param("foto") Foto foto, 
+            @Param("donante") Usuario donante,
+            @Param("beneficiario") Usuario beneficiario,
+            @Param("modificacion") LocalDateTime modificacion);
     
     //Trae todos los productos ordenados por tipo(Donacion/Deseo)
-    @Query("SELECT p FROM Producto ORDER BY p.tipo")
+    @Query("SELECT p FROM Producto p WHERE p.alta = true ORDER BY p.tipo")
     List<Producto> obtenerTodos(); 
     
     //Trae los productos de tipo Donacion
-    @Query("SELECT p FROM Producto WHERE p.tipo = 'Donacion'")
+    @Query("SELECT p FROM Producto p WHERE p.tipo.toString() = 'Donacion' AND p.alta = true")
     List<Producto> obtenerDonaciones(); 
     
     //Trae los productos de tipo Deseo
-    @Query("SELECT p FROM Producto WHERE p.tipo = 'Deseo'")
+    @Query("SELECT p FROM Producto p WHERE p.tipo.toString() = 'Deseo' AND p.alta = true")
     List<Producto> obtenerDeseos(); 
     
     //Trae la lista de productos donados/ofrecidos por un usuario
-    @Query("SELECT p FROM Producto WHERE p.donante.id = :idDonante ORDER BY p.estado")
-    List<Producto> obtenerPorDonante(@Param("idDonante") Integer idDonante); 
+    @Query("SELECT p FROM Producto p WHERE p.donante.id = :idDonante AND p.alta = true ORDER BY p.estado")
+    List<Producto> obtenerPorDonante(@Param("idDonante") Long idDonante); 
     
     //Trae el carrito de un beneficiario
-    @Query("SELECT p FROM Producto WHERE p.beneficiario.id = :idBeneficiario AND p.tipo = 'Donacion'")
-    List<Producto> obtenerCarrito(@Param("idDonante") Integer idDonante);
+    @Query("SELECT p FROM Producto p WHERE p.beneficiario.id = :idBeneficiario AND p.alta = true AND p.tipo.toString() = 'Donacion'")
+    List<Producto> obtenerCarrito(@Param("idBeneficiario") Long idBeneficiario);
     
     //Trae la lista de deseos de un beneficiario
-    @Query("SELECT p FROM Producto WHERE p.beneficiario.id = :idBeneficiario AND p.tipo = 'Deseo'")
-    List<Producto> obtenerListaDeseos(@Param("idDonante") Integer idDonante);
+    @Query("SELECT p FROM Producto p WHERE p.beneficiario.id = :idBeneficiario AND p.alta = true AND p.tipo.toString() = 'Deseo'")
+    List<Producto> obtenerListaDeseos(@Param("idBeneficiario") Long idBeneficiario);
     
     //Habilita un producto
     @Modifying
     @Query("UPDATE Producto p SET p.alta = true WHERE p.id = :id")
-    void habilitar(@Param("id") Integer id);
+    void habilitar(@Param("id") Long id);
+    
+    //Buscador Donación
+    @Query("SELECT p FROM Producto p WHERE p.titulo LIKE '%:busqueda%' AND p.alta = true AND p.tipo.toString() = 'Donacion'")
+    List<Producto> buscarDonacion(@Param("busqueda") String busqueda);
 
+    //Buscador Deseo
+    @Query("SELECT p FROM Producto p WHERE p.titulo LIKE '%:busqueda%' AND p.alta = true AND p.tipo.toString() = 'Deseo'")
+    List<Producto> buscarDeseo(@Param("busqueda") String busqueda);
 }
