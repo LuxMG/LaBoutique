7fb9da5f40d4ca9688a6ce05430b7b87b06f63ae
LuxMG <lucecitagiuliani@gmail.com>
2021-12-01 21:30:16 -0300

    quitar cambios realizados a principalController

--- src/main/java/com/egg/laboutique/controller/PrincipalController.java
+++ src/main/java/com/egg/laboutique/controller/PrincipalController.java
@@ -2,12 +2,14 @@ package com.egg.laboutique.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/")
 public class PrincipalController {
 
-    @GetMapping("/home")
+    @GetMapping
     public ModelAndView inicio() {
         return new ModelAndView("index");
     }
