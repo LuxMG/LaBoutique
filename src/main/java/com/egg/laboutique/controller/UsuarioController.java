92fc5767ad0df94186405099dbb5995976bc117d
Luis <gimenezluis001@gmail.com>
2021-12-13 20:50:47 -0300

    modificaciones varias

--- src/main/java/com/egg/laboutique/controller/UsuarioController.java
+++ src/main/java/com/egg/laboutique/controller/UsuarioController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
@@ -45,9 +46,11 @@ public class UsuarioController {
 
     @GetMapping("/editar/{id}")
     public ModelAndView editarProducto(@PathVariable Long id) {
-        ModelAndView mav = new ModelAndView("nuevo-producto");//refactorizar nombre de html a formulario-producto 
+        ModelAndView mav = new ModelAndView("registro");
         try {
-            mav.addObject("usuario", uService.buscarPorId(id));
+            Usuario usuario = uService.buscarPorId(id);
+            mav.addObject("usuario", usuario);
+            mav.addObject("rol", usuario.getRol());
             mav.addObject("title", "Editar Usuario");
             mav.addObject("action", "modificar");
 
@@ -55,18 +58,34 @@ public class UsuarioController {
             Logger.getLogger(UsuarioController.class.getName()).log(Level.SEVERE, null, ex);
         }
         return mav;
+        
     }
 
     @PostMapping("/modificar")
-    public RedirectView modificar(@RequestBody Usuario usuario) {
+    public RedirectView modificar(@RequestBody Usuario usuario, RedirectAttributes attributtes) {
+        RedirectView redirectView = new RedirectView("/producto/productosTienda");
         try {
             uService.modificarUsuario(usuario.getId(), usuario);
-
+            attributtes.addFlashAttribute("exito", "El registro se ha realizado satisfactoriamente");
         } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return new RedirectView("/usuario");
+            attributtes.addFlashAttribute("error", e.getMessage());
+            attributtes.addFlashAttribute("nombre", usuario.getNombre());
+            attributtes.addFlashAttribute("dni", usuario.getDni());
+            attributtes.addFlashAttribute("email", usuario.getEmail());
+            attributtes.addFlashAttribute("clave", usuario.getClave());
+            switch (usuario.getRol()) {
+                case Beneficiario:
+                    redirectView.setUrl("/registrobeneficiario");
+                    break;
+                case Donante:
+                    redirectView.setUrl("/registrodonante");
+                    break;
+                default:
+                    redirectView.setUrl("/");
+                    break;
+            }
         }
-        return new RedirectView("/usuario");
+        return redirectView;
     }
 
     @PostMapping("/darDeBajaUsuario")
